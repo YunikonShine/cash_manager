286ade6d6cc7a8dc3bea85ac55d22d4fedb27e9f
Vinícius de Moraes <vinicius.moraes9@fatec.sp.gov.br>
2024-10-11 12:21:00 -0300

    Update setup-octo4a.sh

--- setup-octo4a.sh
+++ setup-octo4a.sh
@@ -1,32 +1,55 @@
-#!/bin/sh
-
+#!/bin/bash
 set -e
-# install required dependencies
-apk add py3-pip py3-yaml py3-regex py3-zeroconf py3-netifaces py3-cffi py3-psutil unzip py3-pillow ttyd ffmpeg
+COL='\033[1;32m'
+NC='\033[0m' # No Color
+echo -e "${COL}Setting up klipper"
 
-mkdir -p /root/extensions/ttyd
-cat << EOF > /root/extensions/ttyd/manifest.json
+read -p "Do you have \"Plugin extras\" installed? (y/n): " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo -e "${COL}\nPlease go to settings and install plugin extras${NC}"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+echo -e "${COL}\nInstalling dependencies...\n${NC}"
+# install required dependencies
+apk add py3-cffi py3-greenlet linux-headers can-utils
+pip3 install python-can --break-system-packages
+
+echo -e "${COL}Downloading klipper\n${NC}"
+curl -o klipper.zip -L https://github.com/Klipper3d/klipper/archive/refs/heads/master.zip
+
+echo -e "${COL}Extracting klipper\n${NC}"
+unzip klipper.zip
+rm -rf klipper.zip
+mv klipper-master /klipper
+echo "# replace with your config" >> /root/printer.cfg
+
+mkdir -p /root/extensions/klipper
+cat << EOF > /root/extensions/klipper/manifest.json
 {
-        "title": "Remote web terminal (ttyd)",
-        "description": "Uses port 5002; User root / ssh password"
+        "title": "Klipper plugin",
+        "description": "Requires OctoKlipper plugin"
 }
 EOF
 
-echo "octoprint" > /root/.octoCredentials
-cat << EOF > /root/extensions/ttyd/start.sh
+cat << EOF > /root/extensions/klipper/start.sh
 #!/bin/sh
-ttyd -p 5002 --credential root:\$(cat /root/.octoCredentials) bash
+python3 /klipper/klippy/klippy.py /root/printer.cfg -l /tmp/klippy.log -a /tmp/klippy_uds --break-system-packages
 EOF
 
-cat << EOF > /root/extensions/ttyd/kill.sh
+cat << EOF > /root/extensions/klipper/kill.sh
 #!/bin/sh
-pkill ttyd
+pkill -f 'klippy\.py'
 EOF
-chmod +x /root/extensions/ttyd/start.sh
-chmod +x /root/extensions/ttyd/kill.sh
-chmod 777 /root/extensions/ttyd/start.sh
-chmod 777 /root/extensions/ttyd/kill.sh
+chmod +x /root/extensions/klipper/start.sh
+chmod +x /root/extensions/klipper/kill.sh
+chmod 777 /root/extensions/klipper/start.sh
+chmod 777 /root/extensions/klipper/kill.sh
 
-pip3 install -U packaging --ignore-installed --break-system-packages
-pip3 install https://github.com/feelfreelinux/octo4a-argon2-mock/archive/main.zip --break-system-packages
-touch /home/octoprint/.argon-fix
+cat << EOF
+${COL}
+Klipper installed!
+Please place your own klipper config file at /root/printer.cfg
+Please kill the app and restart it again to see it in extension settings${NC}
+EOF
